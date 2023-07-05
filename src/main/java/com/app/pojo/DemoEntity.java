3b20082a5defdc0d09f400a868d7ed775b23f023
Pranay7160 <hashtagpranay08@gmail.com>
2023-07-05 23:11:50 +0530

    changes made2

--- src/main/java/com/app/pojo/DemoEntity.java
+++ src/main/java/com/app/pojo/DemoEntity.java
@@ -3,6 +3,8 @@ package com.app.pojo;
 public class DemoEntity {
 	public static void main(String[] args) {
 		System.out.println("1st comment");
+		System.out.println("2nd comment");
+		System.out.println("3rd comment");
 	}
 
 }
