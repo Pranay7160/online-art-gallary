007b6275563a2a270f76b6401566ca87edebebd3
Pranay7160 <hashtagpranay08@gmail.com>
2023-07-05 22:32:45 +0530

    adding spring project

---
+++ src/main/java/com/app/DemoSpringProjectForGitApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringProjectForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringProjectForGitApplication.class, args);
+	}
+
+}
